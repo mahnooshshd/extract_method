5a783d71d0686e1668bcca02484e7138d2409c0f
Mahnoosh Shahidi <shahidi@systemnegar.ir>
2021-11-20 01:30:41 +0330

    add json output

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 import sys
+import json
+from alive_progress import alive_bar
+import os
 
 
 def plot_data(classes_data, project_dir_name):
@@ -76,38 +79,54 @@ def main():
     project_dir_name = sys.argv[1]
     call_file_path = '%s/call.txt'%project_dir_name
     project_path = './%s'%project_dir_name
+    project_name = project_dir_name.split('/')[0]
     graph_res = calculate_closeness(call_file_path)
     candidate_functions = get_candidate_functions(graph_res, project_path)
     ranked_refactprings = []
     new_classes_info = []
     line_numbers = []
-    for function in candidate_functions:
-        function_refactoring_res = get_candidate_refactorings(function[-1], function[-2], project_path)
-        if function_refactoring_res is not None:
-            function_refactoring = function_refactoring_res[0]
-            loc = function_refactoring_res[1]
-            method_graph = function_refactoring_res[2]
-            max_rank = -100
-            index = -1
-            for rank in function_refactoring.keys():
-                if rank is not None:
-                    key = list(function_refactoring[rank].keys())[0]
-                    if key > max_rank and key > 0:
-                        max_rank = key
-                        index = rank
-            if max_rank > 0:
-                chosen_refactoring = function_refactoring[index][max_rank]
-                print('chosen refactor for method: %s class: %s'%(function[-1], function[-2]), chosen_refactoring)
-                method_name = get_method_name(chosen_refactoring, method_graph)
-                print('name for chosen refactoring: %s'%method_name)
-                class_data, new_class = define_destination_class(graph_res, function[-1], function[-2])
-                if new_class is not None and class_data is not None:
-                    print('new class is %s --- %s'%(new_class, class_data))
-                    print('----------------------------------')
-                    class_data['name'] = new_class
-                    new_classes_info.append(class_data)
-                    line_numbers.append((loc, len(chosen_refactoring)))
+    json_output = []
+    with alive_bar(len(candidate_functions)) as bar:
+        for function in candidate_functions:
+            bar()
+            function_refactoring_res = get_candidate_refactorings(function[-1], function[-2], project_path)
+            if function_refactoring_res is not None:
+                function_refactoring = function_refactoring_res[0]
+                loc = function_refactoring_res[1]
+                method_graph = function_refactoring_res[2]
+                max_rank = -100
+                index = -1
+                for rank in function_refactoring.keys():
+                    if rank is not None:
+                        key = list(function_refactoring[rank].keys())[0]
+                        if key > max_rank and key > 0:
+                            max_rank = key
+                            index = rank
+                if max_rank > 0:
+                    chosen_refactoring = function_refactoring[index][max_rank]
+                    print('chosen refactor for method: %s class: %s'%(function[-1], function[-2]), chosen_refactoring)
+                    method_name = get_method_name(chosen_refactoring, method_graph)
+                    print('name for chosen refactoring: %s'%method_name)
+                    class_data, new_class = define_destination_class(graph_res, function[-1], function[-2])
+                    if new_class is not None and class_data is not None:
+                        print('new class is %s --- %s'%(new_class, class_data))
+                        print('----------------------------------')
+                        class_data['name'] = new_class
+                        new_classes_info.append(class_data)
+                        line_numbers.append((loc, len(chosen_refactoring)))
+                    output_obj = {
+                        'class': function[-2],
+                        'method': function[-1],
+                        'destination_class': new_class,
+                        'method_name': method_name,
+                        'extracted_line_numbers': chosen_refactoring
+                    }
+                    json_output.append(output_obj)
+                    
 
+    os.makedirs('output', exist_ok=True)
+    output_file = open('output/%s.json'%project_name, 'w+')
+    output_file.write(json.dumps(json_output, indent = 4))
     last = datetime.datetime.now()
     plot_data(new_classes_info, project_dir_name)
     plot_line_numbers(line_numbers, project_dir_name)
